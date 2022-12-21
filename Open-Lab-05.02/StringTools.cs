6abf13f7add331386c57f2398f5258101dc1e443
Roman <vojtus.roman@gmail.com>
2022-12-21 23:01:25 +0100

    ..

--- Open-Lab-05.02/StringTools.cs
+++ Open-Lab-05.02/StringTools.cs
@@ -6,7 +6,13 @@ namespace Open_Lab_05._02
     {
         public string NoYelling(string sentence)
         {
-            throw new NotImplementedException();
+            string result = sentence;
+            while (sentence[sentence.Length-1] == sentence[sentence.Length -2]&&sentence.Length>0 )
+            {
+                result =sentence.Remove(sentence.Length -1 ); 
+            }
+            return result;
+        
         }
     }
 }
